651b0e90c67bc90b3b2843d4836386ceceece1cb
Tayyab-103 <tayyabhameed103@gmail.com>
2023-06-26 21:47:29 +0500

    display on react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+
+let name = "Tayyab";
 
 function App() {
 
@@ -17,7 +18,22 @@ function App() {
       <h1>Hello, {formatName(user)}!</h1>
 
       <div className="blank">lovely</div>
+
+      <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      </nav>
+
+      <div className="container">
+
+        <p>
+          <h1> Hello {name}</h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati consectetur eius reiciendis ducimus cumque ex quis voluptatibus tempore optio iure? Eligendi veniam dignissimos amet. Maxime facilis ea maiores exercitationem doloribus.
+        </p>
+      </div>
     </>
+
   );
 }
 
