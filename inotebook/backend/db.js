1879541d0ab827cae8c4f23dcc32ebb911a60def
Tayyab-103 <tayyabhameed103@gmail.com>
2023-09-26 04:12:46 -0700

    Adding Data Valddation using express validation

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,6 +1,6 @@
  const mongoose = require('mongoose');
  const mongoURI =
-   "mongodb+srv://tayyabhameed103:tayyabhameed103@cwh-45.diw2r3p.mongodb.net/?retryWrites=true&w=majority";
+   "mongodb+srv://tayyabhameed103:tayyabhameed103@cwh-45.diw2r3p.mongodb.net/inotebook?retryWrites=true&w=majority";
 
  mongoose.set("strictQuery", false);
 
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
-
+const { body, validationResult } = require("express-validator");
 
 //create a user using : POST "/api/auth/". Doesn't require Auth
-router.post("/", (req, res) => {
-  // request ki body main kuch bi bejha gaya hy woh main nikal saqta hu
-  console.log(req.body);
+// express validator npm use in [conditions]
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password Must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    // request ki body main kuch bi bejha gaya hy woh main nikal saqta hu
+    // console.log(req.body);
 
-  const user = User(req.body);
-  user.save();
+    // const user = User(req.body);
+    // user.save();
 
-  res.send(req.body);
-});
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // express validator 
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({ error: "please enter a unique value for email", message: err.message });
+      });
+  });
 
 module.exports = router;
