1e0b2c18218597ffa6cccb0170f6a71fb7d4a9e3
Tayyab-103 <tayyabhameed103@gmail.com>
2023-10-02 21:31:12 +0500

    L-49 Hassing Password using bcryptjs and JWT used

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,11 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//JWT signature work ya kisi ko dikhani nahi hy save rakhni hy privacy
+const JWT_SECRET = 'TayyabisaGoodb$oy'
 
 //create a user using : POST "/api/auth/createuser". No Login required
 // express validator npm use in [conditions]
@@ -35,16 +40,34 @@ router.post(
           .status(400)
           .json({ error: "sorry a user with this emailalready exists" });
       }
+      // await is wajha sy kiya kiyu ky bcrypt promise return karta hy
+      const salt = await bcrypt.genSalt(10);
+      // secPass = bcrypt.hash(password, salt);
+       const secPass = await bcrypt.hash(req.body.password, salt);
       //create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+// Data jo ky send karna hy
+const data = {
+  // jo koi bi token mujye wapis dy ga us ko is main bi convert kar saku ga
+  user:{
+    id: user.id
+  }
+}
+      const authToken = jwt.sign(data, JWT_SECRET);
+      // console.log(jwtData)
+      // send user:
+      // res.json(user);
+
+      // res.json({authToken: authToken});
+      // use ES6 same work  
+      res.json({authToken});
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured ")
+      res.status(500).send("Some error occured ");
     }
   }
 );
