6da21bb7032042717c7a90d75fa1b31499581c98
Tayyab-103 <tayyabhameed103@gmail.com>
2023-10-16 11:15:55 +0500

    L-50 Creating Login Endpoint & sending auth token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 //JWT signature work ya kisi ko dikhani nahi hy save rakhni hy privacy
-const JWT_SECRET = 'TayyabisaGoodb$oy'
+const JWT_SECRET = "TayyabisaGoodb$oy";
 
 //create a user using : POST "/api/auth/createuser". No Login required
 // express validator npm use in [conditions]
@@ -43,31 +43,77 @@ router.post(
       // await is wajha sy kiya kiyu ky bcrypt promise return karta hy
       const salt = await bcrypt.genSalt(10);
       // secPass = bcrypt.hash(password, salt);
-       const secPass = await bcrypt.hash(req.body.password, salt);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
       });
-// Data jo ky send karna hy
-const data = {
-  // jo koi bi token mujye wapis dy ga us ko is main bi convert kar saku ga
-  user:{
-    id: user.id
-  }
-}
+      // Data jo ky send karna hy
+      const data = {
+        // jo koi bi token mujye wapis dy ga us ko is main bi convert kar saku ga
+        user: {
+          id: user.id,
+        },
+      };
       const authToken = jwt.sign(data, JWT_SECRET);
       // console.log(jwtData)
       // send user:
       // res.json(user);
 
       // res.json({authToken: authToken});
-      // use ES6 same work  
-      res.json({authToken});
+      // use ES6 same work
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured ");
+      res.status(500).send("Internal Server Error ");
+    }
+  }
+);
+
+//Authenticate a user using : POST "/api/auth/login". No Login required
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password can't be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are errors, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //destructuring used:
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const data = {
+        // jo koi bi token mujye wapis dy ga us ko is main bi convert kar saku ga
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error ");
     }
   }
 );
