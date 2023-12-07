5b6e6122c1b6e8ae040c10d08390fffd02c4f0b1
Tayyab-103 <tayyabhameed103@gmail.com>
2023-12-07 17:06:33 +0500

    L-61 Adding NoteItem in a Separate Note component

---
+++ inotebook/src/components/Notes.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
+import Noteitem from "./Noteitem";
+
+const Notes = () => {
+  const context = useContext(noteContext);
+
+  const { notes, setNotes } = context;
+  return (
+    <div>
+        <h2>Your Notes</h2>
+      <div className="row my-3">
+        {notes.map((note) => {
+          return <Noteitem note={note} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Notes;
--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -4,12 +4,33 @@ import NoteContext from "./noteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "653fb4f6a6cb626ed0d50374",
+      _id: "6551f8881a5694b8573a8f02",
       user: "652c2d95b6ef23c3daf7edf2",
-      title: "My Title",
-      description: "Please wake up early",
-      tag: "personal",
-      date: "2023-10-30T13:51:50.655Z",
+      title: "Money House",
+      description:
+        "What is Moneyhouse in Switzerland? At Moneyhouse, you will find information that you can put to immediate use. The platform offers lots of information free of charge for anyone interested in business. Registered users receive notifications by email, see the network of people involved and the current management of companies.",
+      tag: "Latest Book",
+      date: "2023-11-13T10:20:56.485Z",
+      __v: 0,
+    },
+    {
+      _id: "6551f8881a5694b8573a8f02",
+      user: "652c2d95b6ef23c3daf7edf2",
+      title: "Money House",
+      description:
+        "What is Moneyhouse in Switzerland? At Moneyhouse, you will find information that you can put to immediate use. The platform offers lots of information free of charge for anyone interested in business. Registered users receive notifications by email, see the network of people involved and the current management of companies.",
+      tag: "Latest Book",
+      date: "2023-11-13T10:20:56.485Z",
+      __v: 0,
+    },
+    {
+      _id: "6551f8881a5694b8573a8f02",
+      user: "652c2d95b6ef23c3daf7edf2",
+      title: "Money House",
+      description:
+        "What is Moneyhouse in Switzerland? At Moneyhouse, you will find information that you can put to immediate use. The platform offers lots of information free of charge for anyone interested in business. Registered users receive notifications by email, see the network of people involved and the current management of companies.",
+      tag: "Latest Book",
+      date: "2023-11-13T10:20:56.485Z",
       __v: 0,
     },
     {
